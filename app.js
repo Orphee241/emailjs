9bf42eaa40b189afe40b4fcd94feda5d5fb2c564
Orphee241 <nzienguiakoumbou@gmail.com>
2022-07-28 03:37:45 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+   document.querySelector(".load").style.display = "block"
+ 
+
+   btn.value = 'Envoie...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_jpgfgws';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Envoie';
+      document.querySelector(".load").style.display = "none"
+      alert('Message envoyé!');
+      
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+}); 
+
